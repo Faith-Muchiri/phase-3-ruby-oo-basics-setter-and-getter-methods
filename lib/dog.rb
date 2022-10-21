ec7535f3796d585f7517fdce263df1b8fa05746e
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-21 04:28:30 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
 
+    #getter method
+    def name
+        @name 
+    end
+
+    #setter method
+    def name =(name)
+        @name = name
+    end
+
+    #getter method
+    def breed
+        @breed
+    end
+
+    #setter method
+    def breed =(breed)
+        @breed = breed
+    end
+
+
 end
+
+Poppy = Dog.new
+Poppy.name = "Poppy"
+Poppy.breed ="newSwitz"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
+    def job
+        @job 
+    end
+
+    #setter method
+    def job =(job)
+        @job = job
+    end
 
 end
+liz = Person.new
+liz.job = "Nurse"
