882ca09731fec5bff2e2bdaa32a88463a972598e
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-21 04:30:17 +0300

    complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,25 @@
 class Person
-    def job
-        @job 
-    end
 
-    #setter method
-    def job =(job)
-        @job = job
-    end
+        #getter method
+        def name
+            @name 
+        end
+    
+        #setter method
+        def name =(name)
+            @name = name
+        end
+
+        def job
+            @job 
+        end
+    
+        #setter method
+        def job =(job)
+            @job = job
+        end
+    
+    
 
 end
 liz = Person.new
